39598b052a72abdf6c9307b1e01ee24dd9891815
Yogendar <singhyogendar78@gmail.com>
2021-12-18 02:23:31 +0530

    check deploy

--- model/model.py
+++ model/model.py
@@ -1,40 +1,31 @@
 # Importing the libraries
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
+from sklearn.ensemble import RandomForestRegressor
 
-dataset = pd.read_csv('hiring.csv')
+# loading data from train_data(csv file) with pandas 
+dataset = pd.read_csv('train_data.csv')
 
-dataset['experience'].fillna(0, inplace=True)
+# Dividing data into independent features (X) and dependent feature (y)
+X = dataset.iloc[:, 2:6].values
+y = dataset.iloc[:, -1].values
 
-dataset['test_score'].fillna(dataset['test_score'].mean(), inplace=True)
 
-X = dataset.iloc[:, :3]
-
-#Converting words to integer values
-def convert_to_int(word):
-    word_dict = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8,
-                'nine':9, 'ten':10, 'eleven':11, 'twelve':12, 'zero':0, 0: 0}
-    return word_dict[word]
-
-X['experience'] = X['experience'].apply(lambda x : convert_to_int(x))
-
-y = dataset.iloc[:, -1]
-
-#Splitting Training and Test Set
 #Since we have a very small dataset, we will train our model with all availabe data.
-
-from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
+# here number of decision trees considered are 100.
+regressor = RandomForestRegressor(n_estimators = 100, random_state = 0)
 
 #Fitting model with trainig data
 regressor.fit(X, y)
 
+# Predicting a new result
+x = [[0, 0, 1, 4]]
+# print(regressor.predict(x))
+
 # Saving model to disk
 pickle.dump(regressor, open('model.pkl','wb'))
 
-# Loading model to compare the results
-# model = pickle.load(open('model.pkl','rb'))
-
-# print(model.predict([[2, 9, 6]]))
+# # Loading model to compare the results
+model = pickle.load(open('model.pkl','rb'))
+print(model.predict([[1,1,1,4]]))
